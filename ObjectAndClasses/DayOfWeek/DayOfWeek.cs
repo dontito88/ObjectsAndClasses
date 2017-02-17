02a12362ce46ce4f68d006c2b45f06e8f94d2190
TrifonEstov <don.tito.g@gmail.com>
2017-02-18 01:15:54 +0200

    update

---
+++ ObjectAndClasses/DayOfWeek/DayOfWeek.cs
@@ -0,0 +1,17 @@
+﻿namespace DayOfWeek
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class DayOfWeek
+    {
+        public static void Main()
+        {
+            string inputDate = Console.ReadLine();
+
+            var date = DateTime.ParseExact(inputDate, "d-M-yyyy", CultureInfo.InvariantCulture);
+            Console.WriteLine(date.DayOfWeek);
+        }
+    }
+}
