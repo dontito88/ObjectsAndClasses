9255f3484f4f4d95c234554a6fe86624f7fcd1b6
TrifonEstov <don.tito.g@gmail.com>
2017-02-19 15:47:38 +0200

    Update

---
+++ ObjectAndClasses/BigFactorial/BigFactorial.cs
@@ -0,0 +1,24 @@
+﻿namespace BigFactorial
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+
+    public class BigFactorial
+    {
+        public static void Main()
+        {
+             var n = int.Parse(Console.ReadLine());
+
+            BigInteger result = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                result *= i;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
