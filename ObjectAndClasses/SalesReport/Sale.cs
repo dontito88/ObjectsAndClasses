01678cc25c1c515041d64439ca586bd898a84668
TrifonEstov <don.tito.g@gmail.com>
2017-02-19 21:16:57 +0200

    SaleReport doesn't work properly

---
+++ ObjectAndClasses/SalesReport/Sale.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesReport
+{
+    class Sale
+    {
+        public decimal Price { get; set; }
+        public string Town { get; set; }
+        public string Product { get; set; }
+        public decimal Quantity { get; set; }
+    }
+}
---
+++ ObjectAndClasses/SalesReport/SalesReport.cs
@@ -0,0 +1,46 @@
+﻿namespace SalesReport
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SalesReport
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var list = new List<Sale>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentSaleParts = Console.ReadLine().Split(' ');
+
+                var currentSale = new Sale()
+                {
+                    Town = currentSaleParts[0],
+                    Product = currentSaleParts[1],
+                    Price = decimal.Parse(currentSaleParts[2]),
+                    Quantity = decimal.Parse(currentSaleParts[3])
+                };
+                list.Add(currentSale);
+            }
+
+            var result = new SortedDictionary<string, decimal>();
+
+            foreach (var sale in list)
+            {
+                if (!result.ContainsKey(sale.Town))
+                {
+                    result[sale.Town] = 0;
+                }
+                result[sale.Town] = sale.Price * sale.Quantity;
+            }
+
+            foreach (var kvp in result)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value} ");
+            }
+        }
+    }
+}
